649a8842ee21654b4a601ebabb465f1a10c22aeb
Yann Duran <yd@live.com.au>
2019-02-10 20:42:44 +1100

    refactor: add package class

---
+++ src/vsix/Package.class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Luminous.Code.VisualStudio.Packages;
+
+namespace ExtensibilityLogs
+{
+    class Package : AsyncPackageBase
+    {
+    }
+}
