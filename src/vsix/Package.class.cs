a9e1de825b4a91c83d237d1a7b114bb75bf57d6a
Yann Duran <yd@live.com.au>
2019-02-16 19:31:41 +1100

    refactor(PackageClass)

--- src/vsix/Package.class.cs
+++ src/vsix/Package.class.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Threading;
+using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
 
-using Luminous.Code.VisualStudio.Packages;
-
 namespace ExtensibilityLogs
 {
+    using Luminous.Code.VisualStudio.Packages;
+
+    using static PackageGuids;
+    using static Vsix;
+
+    [InstalledProductRegistration(Name, Description, Version, IconResourceID = 400)]
+    [Guid(PackageString)]
+
     public class PackageClass : AsyncPackageBase
     {
-        public PackageClass() : base()
+        public PackageClass() : base(PackageCommandSet, Name, Description)
         { }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            await base.InitializeAsync(cancellationToken, progress);
+
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
         }
     }
