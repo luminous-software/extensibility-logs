8fd7ddf14a2cbcadb4267c51ce2a50a22d1511af
Yann Duran <yd@live.com.au>
2019-02-16 19:32:55 +1100

    refactor(commands): add ExtensibilityLogsCommand

---
+++ src/vsix/Commands/ExtensibilityLogsCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensibilityLogs.Commands
+{
+    using Luminous.Code.VisualStudio.Commands;
+    using Luminous.Code.VisualStudio.Packages;
+
+    internal abstract class ExtensibilitiesLogCommand : AsyncDynamicCommand
+    {
+        protected ExtensibilitiesLogCommand(AsyncPackageBase package, int id) : base(package, id)
+        { }
+    }
+}
