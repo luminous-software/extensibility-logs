12b17b5e25e670c82fb35ead3a7ef1ee13a992e1
Yann Duran <yd@live.com.au>
2019-03-08 23:28:48 +1100

    refactor(*): add ExtensibilityLogs.Options

---
+++ src/options/GeneralDialogPage.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace ExtensibilityLogs.Options
+{
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    [Guid(Guids.GeneralDialogPageString)]
+    public class GeneralDialogPage : DialogPage
+    {
+    }
+}
---
+++ src/options/Guids.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExtensibilityLogs.Options
+{
+    public static class Guids
+    {
+        internal const string GeneralDialogPageString = "fd68781f-8eb8-4df8-a0f2-aaffa520cd04";
+
+        public static Guid GeneralDialogPage = new Guid(GeneralDialogPageString);
+    }
+}
