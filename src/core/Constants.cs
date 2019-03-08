889868447cd587a261b31dd224a4a2bebe0ecd5d
Yann Duran <yd@live.com.au>
2019-03-08 23:27:25 +1100

    refactor(*): add ExtensibilityLogs.Core

---
+++ src/core/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtensibilityLogs.Core
+{
+    public static class Constants
+
+    {
+        public const string Space = " ";
+
+        public const string General = "General";
+    }
+}
