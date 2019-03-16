a291fbf0cee307a071f53cdd2e30c43473642061
Yann Duran <yd@live.com.au>
2019-03-16 20:52:10 +1100

    feat(GeneralOptionsPage): add ExtensibilityLogsVersion

--- src/options/GeneralDialogPage.cs
+++ src/options/GeneralDialogPage.cs
@@ -17,5 +17,10 @@ namespace ExtensibilityLogs.Options
         [DisplayName(Enable + Space + ExtensibilityLogs)]
         [Description("Allows the whole set of " + ExtensibilityLogs + " features to be turned off together")]
         public bool ExtensibilityLogsEnabled { get; set; } = true;
+
+        [Category(H1 + ExtensibilityLogs)]
+        [DisplayName(Constants.ExtensibilityLogsVersion)]
+        [Description("Installed " + ExtensibilityLogs + " version")]
+        public string ExtensibilityLogsVersion { get; } = Vsix.Version;
     }
 }
