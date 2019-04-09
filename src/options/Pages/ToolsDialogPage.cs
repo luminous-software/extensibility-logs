2cc2a521f37787c1cfbe22995d433c6d7ff57bb8
Yann Duran <yd@live.com.au>
2019-04-09 16:18:09 +1000

    refactor(options): add new options pages
    Related Work Items: #727

---
+++ src/options/Pages/ToolsDialogPage.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace ExtensibilityLogs.Options.Pages
+{
+    using static Core.Constants;
+    using static Guids;
+    using static Constants;
+
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    [Guid(ToolsDialogPageString)]
+    public class ToolsDialogPage : DialogPage
+    {
+        [Category(H1 + Tools + Space + FeatureSet)]
+        [DisplayName(Enable + Space + Tools)]
+        [Description("Allows the whole set of " + Tools + " features to be turned off together")]
+        public bool ToolsEnabled { get; set; } = true;
+
+        [Category(H2 + Features)]
+        [DisplayName(Enable + Space + PathVariables)]
+        [Description("Displays Window's current path variables in a tab document")]
+        public bool PathVariablesCommandEnabled { get; set; } = true;
+
+        [Category(H2 + Features)]
+        [DisplayName(Enable + Space + VisualStudioFolder)]
+        [Description("Opens the current Visual Studio instance's folder")]
+        public bool VisualStudioFolderCommandEnabled { get; set; } = true;
+    }
+}
